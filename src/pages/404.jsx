a3543cccff4e52fe797df0998ebe4b9fd62b818a
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-13 23:55:30 +0530

    Add Tailwind CSS and PostCSS configuration; update 404 page and styles

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,3 +1,9 @@
-function NotFoundPage(){}
+function NotFoundPage(){
+    return(
+        <div className="">
+            <h1>404: Page Not Found</h1>
+        </div>
+    )
+}
 
 export default NotFoundPage
